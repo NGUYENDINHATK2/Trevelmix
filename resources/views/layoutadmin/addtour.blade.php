dc25d92fe9c2528f167ce1f915369fc8cde3541b
AYKDM <minhthai23012002@gmail.com>
2021-11-01 08:59:13 +0800

    cc

--- resources/views/layoutadmin/addtour.blade.php
+++ resources/views/layoutadmin/addtour.blade.php
@@ -222,37 +222,9 @@
         </div>
         <div class="main-panel">
             <div class="content">
+                <input type="text">
                 
-                <a href="" class = "btn btn-success">Thêm tour</a>
-            <table class="table table-striped mt-3">
-											<thead>
-												<tr>
-													<th scope="col">STT</th>
-													<th scope="col">Tên tour</th>
-													<th scope="col">Last</th>
-													<th scope="col">Handle</th>
-												</tr>
-											</thead>
-											<tbody>
-												<tr>
-													<td>1</td>
-													<td>Mark</td>
-													<td>Otto</td>
-													<td>@mdo</td>
-												</tr>
-												<tr>
-													<td>2</td>
-													<td>Jacob</td>
-													<td>Thornton</td>
-													<td>@fat</td>
-												</tr>
-												<tr>
-													<td>3</td>
-													<td colspan="2">Larry the Bird</td>
-													<td>@twitter</td>
-												</tr>
-											</tbody>
-										</table>
+            
             </div>
             <footer class="footer">
                 <div class="container-fluid">
